b87a4a9e73ea009d236ad660cf45c37c915206b3
Lorie-creator <iloriebenezer47@gmail.com>
2024-10-03 17:53:10 +0000

    P1LAB2 - added emojis and decimal places

--- P1LAB2_Ilori.py
+++ P1LAB2_Ilori.py
@@ -28,8 +28,8 @@ meal_total = burger_total + fry_total
 
 # print the receipt
 print("-" * 20)
-print(num_burgers,"burger\t$", burger_total)
-print(num_fries, "fry\t\t$", fry_total)
+print(num_burgers,"burger\t$", format (burger_total, ".2f") )
+print(num_fries, "fry\t\t$", format (fry_total, ".2f") )
 print("-" * 20)
-print("Total\t\t$", meal_total)
+print("Total\t\t$", format(meal_total, ".2f") )
 
