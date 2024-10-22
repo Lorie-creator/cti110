628c0c0849855c592960e6b89b040d6dfc5a43a2
Lorie-creator <iloriebenezer47@gmail.com>
2024-10-22 13:09:41 -0400

    Add files via upload

---
+++ P2LAB2_IloriEbenezer.py
@@ -0,0 +1,32 @@
+# CTI 110
+# P2LAB2
+# Ilori
+# 10/17/2024
+
+# using lists and loops to draw
+
+import turtle
+t = turtle.Turtle()
+# remeber pensize, pencolor
+t.pensize(10)
+t.pencolor("red")
+
+#simple loop
+for lenght in [10, 20, 10, 20, 10, 20, 10, 20, 10, 20]:
+    t.forward(65)
+    t.right(50)
+
+t. pencolor("blue")
+for length in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
+    t.forward(120)
+    t.right(90)
+
+
+sides = 40
+length = 400
+angle = 360 / 360
+
+for color in ["red", "green", "blue", "yellow", "brown"]:
+    t.pencolor("green")
+    t.forward(130)
+    t.right(120)
