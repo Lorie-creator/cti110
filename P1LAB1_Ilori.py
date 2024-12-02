f6c82cdcb235a4cd7541ddb0004ff228fcb51683
Lorie-creator <iloriebenezer47@gmail.com>
2024-12-01 22:10:34 -0500

    Add files via upload

---
+++ P1LAB1_Ilori.py
@@ -0,0 +1,32 @@
+# CTI 110
+# P1LAB - My First Program
+# Ebenezer
+# 9/24/24
+
+# example of formatting
+# comments are # or triple quotes
+"""
+
+print(' The cow says "Moo" ')
+print("Newline\n is kind of like \n<br>\n")
+print("\tTabs space out \t\twords")
+print("\\ is the backlsh")
+"""
+
+print("Hello!")
+# greet the user by nmae
+# TODO: ask their name
+#print("What's your name?")
+#print("Enter first name:")
+first = input ("What's your first name? ")
+#print("Ente last name:")
+last = input ("What's your last name? ")
+
+
+# say their name
+print("Nice to meet you, ", first, last)
+
+# ask them if they like stuff
+#print("What do you like?")
+topic = input("What stuff do you like? ")
+print("Cool! Ilike", topic, "too.")
